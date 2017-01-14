13c8cdaa24cb17fa3c04063c3234849348bd26c2
navya1111 <navyaparuchuri11@gmail.com>
2017-01-13 21:15:31 -0600

    initial commit

---
+++ maps.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+func main(){
+	x := make(map[string] int)
+	x["a"]= 6
+	x["e"]= 5
+	x["i"]= 4
+	x["o"]= 3
+	x["u"]= 2
+	if value,ok:=x["o"]; ok{
+		fmt.Println(value,ok)
+	}
+
+	//fmt.Println(value)
+	//fmt.Println(ok)
+	fmt.Println(x)
+	fmt.Println(len(x))
+	v := x
+	fmt.Println(v)
+}
