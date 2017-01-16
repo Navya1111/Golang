6c6d689913ee82cb3e681d653b114b631d237f7d
navya1111 <navyaparuchuri11@gmail.com>
2017-01-16 13:00:46 -0600

    initial commit

---
+++ variables.go
@@ -0,0 +1,23 @@
+package main
+import "fmt"
+func main(){
+	 a := 5
+	fmt.Println(a)
+	//fmt.Println(b)
+	 num ,val  := 2 , 10
+	var c int = num + val
+	//var d string = a
+	fmt.Println(c)
+	//fmt.Println(d)
+
+	abc := "hello"
+	fmt.Println(abc)
+        //var x string
+	x := "uefh"
+	fmt.Println(x)
+	x = "hbejh"
+	fmt.Println(x)
+
+
+
+}
