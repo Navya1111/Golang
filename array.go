31809ce6530e9e69de885a256881b9b752420034
navya1111 <navyaparuchuri11@gmail.com>
2017-01-16 12:47:36 -0600

    initial commit

---
+++ array.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+func main(){
+	var a [5]float64
+	fmt.Println(a)
+	a[3] = 10
+	fmt.Println(a)
+	fmt.Println(a[4])
+	b := [4]float64{3,6,9,1}
+        fmt.Println(b)
+
+
+}
