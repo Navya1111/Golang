ecbf656f0a885c75211fcddf66a62c0e991da9ea
navya1111 <navyaparuchuri11@gmail.com>
2017-01-16 12:57:56 -0600

    initial commit

---
+++ Values.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+func main(){
+	fmt.Println("2 + 2 =", 2 + 2)
+	fmt.Println("7.0 / 2.5 =", 7.0 / 2.5)
+	fmt.Println("om" + "sai")
+	fmt.Println("2 - 2 =",2 - 2)
+	fmt.Println(true || false)
+	fmt.Println(true && true)
+}
